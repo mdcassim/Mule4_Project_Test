52a58601d69bb42e1b235f77b77214a173778a27
Mohamed Cassim <mdcassimsait@gmail.com>
2019-12-11 17:25:43 +0530

    Update and rename Demo.java to Testing File

--- src/main/java/Demo.java
+++
@@ -1,8 +0,0 @@
-package com.javahungry; 
-public class Demo {
-
-    public static void main(String args[]) 
-    {
-        System.out.println(" Mule4 Project Deploying through Jenkins pipeline ");       
-    }
-}
